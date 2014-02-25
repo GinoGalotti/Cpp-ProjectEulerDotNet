e22d212dd98deae939f0a4e055bb09b2d9b8f165
Surth (GinoGalotti) <surthlover@gmail.com>
2014-02-25 21:14:23 +0100

    Update Problem3.cpp

--- Problem3.cpp
+++ Problem3.cpp
@@ -1,47 +1,39 @@
-/** TODO: QUE FUNCIONE COJONEH
+/**
  * Solution by Luis Galotti
  * The prime factors of 13195 are 5, 7, 13 and 29.
 
 What is the largest prime factor of the number 600851475143 ?
 */
- 
-#include <iostream>
-bool isPrime (long long number);
 
-bool isPrime (long long number){
-    long long i;
-    
-	for (i=2; i<number; i++)
+#include <stdio.h>
+bool isPrime (unsigned long long number);
+bool isPrime (unsigned long long number){
+    unsigned long long i;
+
+	for (i=2; (i * i)<number; i++)
 	{
 		if (number % i == 0)
 		{
 			return false;
 		}}
-	return true;	
- 
-}    
+	return true;
 
-
+}
 using namespace std;
-
 int main()
 {
-    long long solution = 0;
-    long long number = 600851475142;
-    long long max = 600851475143;
-    bool found = false;
-    while (!found && number > 1){
-        if (max % number == 0){
-            if (isPrime(number)) 
-                found = true;}
-        number--;
+    unsigned long long solution = 0;
+    const unsigned  long long number = 600851475143;
+    //This way we check just until square root of number;
+    for (unsigned long long i = 2; (i * i) < number; i++){
+        if (number % i == 0){
+        unsigned long long aux = i;
+        if (isPrime(aux) and aux > solution) solution = aux;
+        unsigned long long aux2 = number / i;
+        if (isPrime(aux2) and isPrime(aux) and aux2 > solution) solution = aux2;}
     }
-    solution = number;
-    if (solution == 0) 
-        printf("600851475143 is not prime");
-    else {
-        printf("pedo");
-        printf("Solution =  %l", solution);} 
-   
+
+        printf("Solution =  %llu", solution);
+
    return 0;
 }
