82644db97c9b541764883bc86f653108155f2c0e
Surth (GinoGalotti) <surthlover@gmail.com>
2014-02-26 20:13:57 +0100

    Update Problem9.cpp

--- Problem9.cpp
+++ Problem9.cpp
@@ -16,32 +16,26 @@ int* findTriplet (int addition){
     int* pointer = triplet;
 
     for (int i = addition-3; i > (addition / 3) + 2;  i--){
-        
-        int j = (i <= addition /2) ? i : addition /2;
-        
-        for (j; j <= addition / 3 + 1 ; j--){
-            int z = (j < (addition/3)) ? j : (addition/3-1);
-            
-            for (z; z <= (addition/3-1); z--){
-                
-                if (i + j + z == 1000){
-                    if (z*z == j*j + i*i){
-                    triplet[0] = i;
-                    triplet[1] = j;
-                    triplet[2] = z;
-                    return pointer;}
-                }
+
+        int j = (i <= addition - i) ? (i-1) : (addition - i - 1);
+
+        for (j; j > 2 ; j--){
+            int z = addition - i - j;
+            if ((i + j + z) == addition){
+                if (z*z == (j*j + i*i)){
+                triplet[0] = i;
+                triplet[1] = j;
+                triplet[2] = z;
+                return pointer;}
             }
         }
     }
-    
-    
     return pointer;
-}    
+}
 using namespace std;
 
 int main()
-{   
+{
     int *triplet, product;
     triplet = findTriplet(1000);
     product = triplet[0] * triplet[1] * triplet[2];
